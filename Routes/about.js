7c24025575d4ffaa9b49042e1cb2cf85ddd2f6e7
Louis Douglas <louis.douglas.2017@mumail.ie>
2021-01-29 21:48:14 +0000

    added js files to the html

--- Routes/about.js
+++ Routes/about.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/',(req, res)=>{
+    console.log(77)
     res.render('about')
 })
 
--- Server.js
+++ Server.js
@@ -9,13 +9,12 @@ app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 
-app.use(handler)
-app.use('/', require('./Routes/home'))
+app.use('/home', require('./Routes/home'))
+app.use('/about', require('./Routes/about'))
 
-function handler(req, res, next){
-    app.use(req.originalUrl,require('./Routes/about'))
-    next()
-}
+app.get('/', (req, res)=>{
+    res.redirect('/home')
+})
 
 function startServer(err, res){
     if (err) throw err
